61784f0d333a12866c2bac5c71006dd915d01eb5
Raoul Cornelissen <raoul@raoulcornelissen.com>
2016-10-21 03:16:51 +0200

    Made layout for first activity: MainActivity. Also made button that starts second activity: CountActivity

---
+++ app/src/main/java/com/raoulcornelissen/mcd/mcapp/CountActivity.java
@@ -0,0 +1,17 @@
+package com.raoulcornelissen.mcd.mcapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by raoul on 21-10-2016.
+ */
+
+public class CountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
+    }
+
+}
